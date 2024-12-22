552b530c575fb95471d9d465358c01daf489c92a
Redeat Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2024-12-22 23:29:49 +0300

    Even numbers between 1 and 20

--- task.py
+++ task.py
@@ -1,10 +1,4 @@
-def add_numbers():
-    numbers=[]
-    summ=0
-    count=int(input("Enter how many numbers you want to add:"))
-    for i in range(count):
-        number=int(input(f'Enter number:{i+1}'))
-        numbers.append(number)
-    summ=sum(numbers)
-    return f'The total sum of the numbers is: {summ}'
-print(add_numbers())
+def even_numbers():
+    for i in range(2,20,2):
+        print(i)
+even_numbers()
