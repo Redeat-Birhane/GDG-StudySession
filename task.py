88b5659b4d99a8dc7cfccce03819c229695ce0fa
Redeat Birhane <149287082+Redeat-Birhane@users.noreply.github.com>
2024-12-22 23:39:31 +0300

    The largest number in a given list

--- task.py
+++ task.py
@@ -1,4 +1,8 @@
-def even_numbers():
-    for i in range(2,20,2):
-        print(i)
-even_numbers()
+def largest_number():
+    numbers=[]
+    count=int(input("Enter the number of  elements you wish to have in a list:"))
+    for i in range(count):
+        number=int(input(f'Enter number {i+1}'))
+        numbers.append(number)
+    return f'The largset number is {max(numbers)}'
+print(largest_number())
