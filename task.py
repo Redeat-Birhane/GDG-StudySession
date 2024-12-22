71ec3b482d6fb90e69a65adbcfc7acc05257cafc
Redeat-Birhane <redeatbirhane2@gmail.com>
2024-12-22 23:41:58 +0300

    task.py

--- task.py
+++ task.py
@@ -4,5 +4,5 @@ def largest_number():
     for i in range(count):
         number=int(input(f'Enter number {i+1}'))
         numbers.append(number)
-    return f'The largset number is {max(numbers)}'
+    return f'The largest number is {max(numbers)}'
 print(largest_number())
